b47c65d804ff37b2e7dee048a56d6a90e92e6b2a
Debasnana Pattanaik <pattanaikd1998@gmail.com>
2025-08-03 13:12:33 -0700

    program

---
+++ src/main/java/com/java/strings/SumOfFirstTwo.java
@@ -0,0 +1,21 @@
+package com.java.strings;
+
+/*
+ * You are given with list of numbers. Find the sum of integers value of first 2
+ * 87,45,35,74,325,98
+ *  //87 + 45*/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+public class SumOfFirstTwo
+{
+    public class SumofNumbers{
+        public static void main(String[] args) {
+            List<Integer> numbersList = List.of(87,45,35,74,325,98); //87 + 45
+            System.out.println(numbersList.stream().limit(2).mapToInt(Integer::intValue).sum());
+        }
+    }
+}
